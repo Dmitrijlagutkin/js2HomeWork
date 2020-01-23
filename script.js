47c3617c77124fade40931364202b42a486208f3
Dmitrij Lagutkin <dmitrijlagutkin@gmail.com>
2020-01-23 19:36:55 +0100

    homework done

---
+++ script.js
@@ -0,0 +1,47 @@
+let userInput = prompt('Enter any symbol')
+
+if (userInput === null) {
+	alert('Ввод был отменен')
+} else {
+	userInput = userInput.length > 1 ? " " : userInput
+
+	switch (userInput) {
+		case " ":
+			userInput = " "
+			alert(!!userInput, typeof userInput)
+			break
+		case "":
+			userInput = " "
+			alert(!!userInput, typeof userInput)
+			break
+		default:
+			userInput = userInput.toLowerCase()
+			switch (userInput) {
+				case "a":
+				case "e":
+				case "o":
+					alert(userInput.toUpperCase())
+					break
+				case "x":
+				case "y":
+					alert(userInput + userInput)
+					break
+				case "s":
+				case "d":
+				case "q":
+				case "m":
+					let randomFirstNumber = Math.random()
+					let randomSecondNumber = Math.random()
+					let randomNumberResult = Math.trunc(randomFirstNumber + randomSecondNumber)
+					let randomNumber = randomNumberResult === 0 ? 1 : randomNumberResult
+					alert(randomNumber)
+					break
+				default:
+					let randomX = Math.random()
+					let randomY = Math.random()
+					let randomZ = Math.random()
+					console.log(randomX, randomY, randomZ)
+					alert(`max value = ${Math.max(randomX, randomY, randomZ)}`)
+			}
+	}
+}
